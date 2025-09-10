2bba88f7130f867757df84b72b144749335f6be2
rksvw <ritik.vw98@gmail.com>
2025-09-10 20:31:35 +0530

    Section 1 [declaration and 1st program]

--- hello-args.go
+++ hello-args.go
@@ -1,3 +1,4 @@
+// Echo1 prints its comman-line argumnets.
 package main
 
 import (
@@ -6,12 +7,29 @@ import (
 )
 
 func main() {
-	var s, sep string
+	// var s, sep string
 
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
+	// for i := 1; i < len(os.Args); i++ {
+	// 	s += sep + os.Args[i]
+	// 	sep = " "
+	// }
+
+	// fmt.Println(s)
+
+	echo2()
+}
+
+func echo2() {
+	// := short variable declaration ( used to automatic detect the data type based on value)
+	s, sep := "", ""
+
+	// Range := Produce a pair of values: index and value of the element at that index
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
 		sep = " "
 	}
+
+	// _ : blank identifier ( require variable name but not logic )
 
 	fmt.Println(s)
 }
--- hello.go
+++ hello.go
@@ -1,8 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
-func dec() {
-	var hello = 5
-	fmt.Println("Hello, Go on Ubuntu!", hello)
+func identifier() {
+	// Short variable declaration, used only within a function (not for package-level variables).
+	s := "Hello"
+
+	// Default initialization to the zero value of strings -> ""
+	var st string
+
+	// Rarely used except when declaring multiple variables
+	var str = "Hello str"
+
+	// explicit about the variable's tpe
+	var istr string = " Hello iStr"
+
+	st = "Hello st of string"
+
+	println(s, st, str, istr)
+
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
+
+func arg() {
+	fmt.Println(os.Args[1:])
+}
+
+func main() {
+	// identifier()
+	arg()
 }
